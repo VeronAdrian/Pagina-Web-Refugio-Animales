6256f560463aa08895f108f302e5bcc12ea8d7d3
Veron Adrian <adrianveron4@hotmail.com>
2023-05-14 19:27:46 -0300

    Login, registro y perfil

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 const user = JSON.parse(localStorage.getItem('login_success')) || false
+
+if(user){
+    console.log("Logueado como "+user.name)
+    document.getElementById('loginRegister').style.display = 'none'
+}
+else{
+    document.getElementById('logout').style.display = 'none'
+    document.getElementById('profile').style.display = 'none'
+}
+
+const logout = document.getElementById('logout')
+logout.addEventListener('click', (e)=>{
+    localStorage.removeItem('login_success')
+    return window.location="index.html"
+})
